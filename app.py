614873913d9e734bd428396e2c13de20531f6910
Ayush Gupta <ayush2020cs159@abesit.edu.in>
2023-07-04 23:09:43 +0530

    Enable Edit functionality

--- app.py
+++ app.py
@@ -141,6 +141,7 @@ def edit(id):
 
             with app.app_context():
                 # Update the student in the database
+                db.session.merge(student)
                 db.session.commit()
                 print(f"Updated student record: id={form_id}, name={name}, city={city}, addr={addr}, pin={pin}")
                 # Check if the transaction was committed
@@ -175,37 +176,3 @@ if __name__ == '__main__':
 
     # Start the Flask application
     app.run(debug=True)
-
-#     ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-#     from flask import Flask, render_template, request, redirect, url_for, flash
-# from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin
-# from flask_sqlalchemy import SQLAlchemy
-
-# app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'secret_key'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-# db = SQLAlchemy(app)
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-
-# # Define the User model
-# class User(UserMixin, db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     password = db.Column(db.String(120), nullable=False)
-
-#     def __repr__(self):
-#         return '<User %r>' % self.username
-
-# # Define the login route
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         username = request.form['username']
-#         password = request.form['password']
-#         user = User.query.filter_by(username=username).first()
-#         if user and user.password == password:
-#             login_user(user)
-#             flash('Login successful')
-#             return redirect(url_for('show_all'))
